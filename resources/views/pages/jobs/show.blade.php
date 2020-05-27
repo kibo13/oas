7d1e3b4c8aa8dca2577c3e27cbbe88388c89021c
unknown <ARM-1@ARM1.cenki.ru>
2020-05-27 13:22:32 +0100

    changed form for viewing records from table of job

--- resources/views/pages/jobs/show.blade.php
+++ resources/views/pages/jobs/show.blade.php
@@ -1,57 +1,83 @@
 @extends('layouts.master')
 
 @section('content')
-<div class="overflow-hidden pt-4 py-2">
+<div class="pt-4 py-2">
 
-	<h2 class="mb-3 pb-2 border-bottom">Просмотр записи</h2>
+	<h4 class="mb-2 pb-2 border-bottom">Код работы №{{ $job->id }}</h4>
 
-	<div class="bk-blank row">
+	<div class="row p-0 m-0">
 
-		<div class="bk-blank__item col-md-12 bgc-light">
-			<h4 class="bk-blank__title m-0">Заявка</h4>
-			<p class="bk-blank__text m-0">#{{ $job->id }}</p>
+		<div class="col-md-12 py-0 px-1">
+			<p class="font-weight-bold border-bottom mb-0 px-1">
+				Предприятие
+			</p>
+
+			<p class="px-1">
+				{{ $job->organization->name }}
+			</p>
 		</div>
 
-		<div class="bk-blank__item col-md-12">
-			<h4 class="bk-blank__title m-0">Предприятие</h4>
-			<p class="bk-blank__text m-0">{{ $job->organization->name }}</p>
+		<div class="col-md-12 py-0 px-1">
+			<p class="font-weight-bold border-bottom mb-0 px-1">
+				Вид работ
+			</p>
+
+			<p class="px-1">
+				{{ $job->type_job }}
+			</p>
 		</div>
 
-		<div class="bk-blank__item col-md-12">
-			<h4 class="bk-blank__title m-0">Вид работ</h4>
-			<p class="bk-blank__text m-0">{{ $job->type_job }}</p>
+		<div class="col-md-12 py-0 px-1">
+			<p class="font-weight-bold border-bottom mb-0 px-1">
+				Тип отключения
+			</p>
+
+			<p class="px-1">
+				{{ $job->type_off }}
+			</p>
 		</div>
 
-		<div class="bk-blank__item col-md-12">
-			<h4 class="bk-blank__title m-0">Тип отключения</h4>
-			<p class="bk-blank__text m-0">{{ $job->type_off }}</p>
+		<div class="col-md-12 py-0 px-1">
+			<p class="font-weight-bold border-bottom mb-0 px-1">
+				Дата и время начало работы
+			</p>
+
+			<p class="px-1">
+				{{ date('d.m.Y', strtotime($job->date_on)) }}г. {{ date('H:s', strtotime($job->time_on)) }}ч.
+			</p>
 		</div>
 
-		<div class="bk-blank__item col-md-12">
-			<h4 class="bk-blank__title m-0">Начало работы</h4>
-			<p class="bk-blank__text m-0">{{ date('d.m.Y', strtotime($job->date_on)) }}</p>
+		<div class="col-md-12 py-0 px-1">
+			<p class="font-weight-bold border-bottom mb-0 px-1">
+				Дата и время завершения работы
+			</p>
+
+			<p class="px-1">
+				{{ date('d.m.Y', strtotime($job->date_off)) }}г. {{ date('H:s', strtotime($job->time_off)) }}ч.
+			</p>
 		</div>
 
-		<div class="bk-blank__item col-md-12">
-			<h4 class="bk-blank__title m-0">Окончание работы</h4>
-			<p class="bk-blank__text m-0">{{ date('d.m.Y', strtotime($job->date_off)) }}</p>
+		<div class="col-md-12 py-0 px-1">
+			<p class="font-weight-bold border-bottom mb-0 px-1">
+				Место проведения работы
+			</p>
+
+			<p class="px-1">
+				{{ $job->street->name }} {{ $job->num_home }}{{ $job->num_corp }}
+			</p>
 		</div>
 
-		<div class="bk-blank__item col-md-12">
-			<h4 class="bk-blank__title m-0">Место проведения работы</h4>
-			<p class="bk-blank__text m-0 p-0">{{ $job->street->name }} {{ $job->num_home }} {{ $job->num_corp }}</p>
+		<div class="col-md-12 py-0 px-1">
+			<p class="font-weight-bold border-bottom mb-0 px-1">
+				Описание работы
+			</p>
+
+			<p class="px-1">
+				{{ $job->desc }}
+			</p>
 		</div>
-
-		<div class="bk-blank__item col-md-12">
-			<h4 class="bk-blank__title m-0">Описание работы</h4>
-			<p class="bk-blank__text m-0">{{ $job->desc }}</p>
-		</div>
-
-
 
 	</div>
-
-
 
 	<a href="{{ route('jobs.index') }}" class="btn btn-outline-secondary">Назад</a>
 
