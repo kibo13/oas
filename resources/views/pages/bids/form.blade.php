7924886973fc96c1f36d3cdb720b46373424bb81
Boris Kim <kimboris1310@gmail.com>
2020-05-29 03:04:14 +0500

    added few changes to file form.blade.php(bids)

--- resources/views/pages/bids/form.blade.php
+++ resources/views/pages/bids/form.blade.php
@@ -26,11 +26,11 @@
 						<h6 class="col-md-12 border-bottom py-1 pl-0">Дата и время поступления</h6>
 
 						<div class="col-md-2 form-group mb-2 pl-0">
-							<input name="date_on" type="date" class="form-control bk-form__input" value="{{ old('date_on', isset($job) ? $job->date_on : null) }}" required>
+							<input name="date_in" type="date" class="form-control bk-form__input" value="{{ old('date_in', isset($bid) ? $bid->date_in : null) }}" required>
 						</div>
 
 						<div class="col-md-2 form-group mb-2 pl-0">
-							<input name="time_on" type="time" class="form-control bk-form__input" value="{{ old('time_on', isset($job) ? $job->time_on : null) }}" required>
+							<input name="time_in" type="time" class="form-control bk-form__input" value="{{ old('time_in', isset($bid) ? $bid->time_in : null) }}" required>
 						</div>
 					</div>
 
@@ -42,7 +42,7 @@
 							<select name="street_id" id="street" class="form-control bk-form__input">
 								<option disabled selected>Выберите улицу</option>
 								@foreach($streets as $street)
-								<option value="{{ $street->id }}" @isset($worker) @if($worker->street_id == $street->id)
+								<option value="{{ $street->id }}" @isset($bid) @if($bid->street_id == $street->id)
 									selected
 									@endif
 									@endisset
@@ -55,24 +55,24 @@
 
 						<div class="col-md-1 form-group mb-2 pl-0">
 							<label for="num_home" class="bk-form__label mb-0">Дом</label>
-							<input id="num_home" min="1" max="150" type="number" class="form-control bk-form__input" name="num_home" required value="{{ old('num_home', isset($worker) ? $worker->num_home : null) }}">
+							<input id="num_home" min="1" max="150" type="number" class="form-control bk-form__input" name="num_home" required value="{{ old('num_home', isset($bid) ? $bid->num_home : null) }}">
 						</div>
 
 						<div class="col-md-1 form-group mb-2 pl-0">
 							<label for="num_flat" class="bk-form__label mb-0">Квартира</label>
-							<input id="num_flat" min="1" max="150" type="number" class="form-control bk-form__input" name="num_flat" required value="{{ old('num_flat', isset($worker) ? $worker->num_flat : null) }}">
+							<input id="num_flat" min="1" max="150" type="number" class="form-control bk-form__input" name="num_flat" required value="{{ old('num_flat', isset($bid) ? $bid->num_flat : null) }}">
 						</div>
 					</div>
 
 					<div class="col-md-12 row m-0 p-0">
 						<div class="col-md-2 form-group mb-2 pl-0">
 							<label for="last_name" class="bk-form__label mb-0">Фамилия</label>
-							<input id="last_name" type="text" class="form-control bk-form__input" name="last_name" required placeholder="Введите фамилию сотрудника" value="{{ old('last_name', isset($worker) ? $worker->last_name : null) }}">
+							<input id="last_name" type="text" class="form-control bk-form__input" name="last_name" required placeholder="Введите фамилию сотрудника" value="{{ old('last_name', isset($bid) ? $bid->last_name : null) }}">
 						</div>
 
 						<div class="col-md-2 form-group mb-2 pl-0">
-							<label for="first_name" class="bk-form__label mb-0">Имя</label>
-							<input id="first_name" type="text" class="form-control bk-form__input" name="first_name" required placeholder="Введите имя сотрудника" value="{{ old('first_name', isset($worker) ? $worker->first_name : null) }}">
+							<label for="phone" class="bk-form__label mb-0">Телефон</label>
+							<input id="phone" type="text" class="form-control bk-form__input" name="phone" required placeholder="Введите имя сотрудника" value="{{ old('phone', isset($bid) ? $bid->phone : null) }}">
 						</div>
 					</div>
 
@@ -93,7 +93,7 @@
 							<select name="type_id" id="type" class="form-control bk-form__input">
 								<option disabled selected>Выберите тип</option>
 								@foreach($types as $type)
-								<option value="{{ $type->id }}" @isset($promiser) @if($promiser->type_id == $type->id)
+								<option value="{{ $type->id }}" @isset($bid) @if($bid->type_id == $type->id)
 									selected
 									@endif
 									@endisset
@@ -111,7 +111,7 @@
 							<select name="branch_id" id="branch_id" class="form-control bk-form__input">
 								<option disabled selected>Выберите отдел</option>
 								@foreach($branches as $branch)
-								<option value="{{ $branch->id }}" @isset($worker) @if($worker->branch_id == $branch->id)
+								<option value="{{ $branch->id }}" @isset($bid) @if($bid->branch_id == $branch->id)
 									selected
 									@endif
 									@endisset
@@ -125,11 +125,28 @@
 
 					<div class="col-md-12 row m-0 p-0">
 						<div class="col-md-4 form-group mb-2 pl-0">
-							<label for="last_name" class="bk-form__label mb-0">Фамилия</label>
-							<input id="last_name" type="text" class="form-control bk-form__input" name="last_name" required placeholder="Введите фамилию сотрудника" value="{{ old('last_name', isset($worker) ? $worker->last_name : null) }}">
+							<label for="defect_id" class="bk-form__label mb-0">Неисправность</label>
+							<select name="defect_id" id="defect_id" class="form-control bk-form__input">
+								<option disabled selected>Выберите неисправность</option>
+
+								@foreach($defects as $defect)
+								<option 
+									value="{{ $defect->id }}" 
+									@isset($bid) 
+										@if($bid->defect_id == $defect->id)
+											selected
+										@endif
+									@endisset
+								>
+									{{ ucfirst($defect->desc) }}
+								</option>
+								@endforeach
+								
+							</select>
+
 						</div>
 
-						
+
 					</div>
 
 				</div>
